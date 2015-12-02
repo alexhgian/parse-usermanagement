a8ff11be38f97a280cefe3b98de01feae9173852
xxvii27 <llesmana@ucsd.edu>
2015-12-02 14:16:48 -0800

    clean

--- cloud/main.js
+++ cloud/main.js
@@ -1,44 +1,5 @@
-var test = require('cloud/controller/role.js');
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
 Parse.Cloud.useMasterKey();
-// Parse.Cloud.define("test", function(request, response) {
-//     console.log(test);
-//     response.success(test);
-// });
-//
-//
-// Parse.Cloud.define("getUser", function(request, response) {
-//     var currentUser = Parse.User.current();
-//     console.log('currentUser');
-//     console.log(currentUser);
-//
-//     var uid = request.params.userId;
-//     var User = Parse.Object.extend("_User");
-//     var query = new Parse.Query(User);
-//
-//     query.get(uid, {
-//         success: function(data) {
-//             // The object was retrieved successfully.
-//             response.success(data);
-//         },
-//         error: function(object, error) {
-//             // The object was not retrieved successfully.
-//             // error is a Parse.Error with an error code and message.
-//             response.success(error);
-//         }
-//     });
-// });
-// Parse.Cloud.define("current", function(request, response) {
-//     var currentUser = Parse.User.current();
-//     console.log('currentUser');
-//     console.log(currentUser);
-//     // The object was retrieved successfully.
-//     response.success(currentUser);
-//
-// });
-
 
 // EndPoint:
 //  https://api.parse.com/1/functions/deleteUser
